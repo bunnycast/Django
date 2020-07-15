954de3fe67190b1760d410affa9c15c6cc267a42
bunnycast <berzzubunny@gmail.com>
2020-07-16 00:50:37 +0900

    feat

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -4,59 +4,61 @@ from django.contrib.auth.hashers import make_password, check_password
 from .models import Fcuser
 from .forms import LoginForm
 
+
 # Create your views here.
 
 
 def home(request):
-  return render(request, 'home.html')
-  # user_id = request.session.get('user')
+    return render(request, 'home.html')
+    # user_id = request.session.get('user')
 
-  # if user_id:
-  #   fcuser = Fcuser.objects.get(pk=user_id)
-
+    # if user_id:
+    #   fcuser = Fcuser.objects.get(pk=user_id)
 
 
 def logout(request):
-  if request.session.get('user'):
-    del(request.session['user'])
+    if request.session.get('user'):
+        del (request.session['user'])
 
-  return redirect('/')
+    return redirect('/')
+
 
 def login(request):
-  if request.method == 'POST':
-    form = LoginForm(request.POST)
-    if form.is_valid():
-      request.session['user'] = form.user_id
-      return redirect('/')
-  else:
-    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm()
 
-  return render(request, 'login.html', {'form': form})
+    return render(request, 'login.html', {'form': form})
+
 
 def register(request):
-  if request.method == 'GET':
-    return render(request, 'register.html')
-  elif request.method == 'POST':
-    username = request.POST.get('username', None)
-    useremail = request.POST.get('useremail', None)
-    password = request.POST.get('password', None)
-    re_password = request.POST.get('re-password', None)
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
 
-    res_data = {}
+        res_data = {}
 
-    if not (username and useremail and password and re_password):
-      res_data['error'] = '모든 값을 입력해야합니다.'
-    
-    elif password != re_password:
-      res_data['error'] = '비밀번호가 다릅니다.'
+        if not (username and useremail and password and re_password):
+            res_data['error'] = '모든 값을 입력해야합니다.'
 
-    else:
-      fcuser = Fcuser(
-        username=username,
-        useremail=useremail,
-        password=make_password(password) # 비밀번호 암호화 저장 모듈
-        )
-        
-      fcuser.save()
-    
-    return render(request, 'register.html', res_data)
+        elif password != re_password:
+            res_data['error'] = '비밀번호가 다릅니다.'
+
+        else:
+            fcuser = Fcuser(
+                username=username,
+                useremail=useremail,
+                password=make_password(password)  # 비밀번호 암호화 저장 모듈
+            )
+
+            fcuser.save()
+
+        return render(request, 'register.html', res_data)
